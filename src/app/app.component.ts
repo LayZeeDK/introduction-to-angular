28a76a83f995a13c37e00798f181f25c26053a75
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2024-11-30 19:03:37 +0100

    feat: add hello world component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
+// app.component.ts
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HelloWorldComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'introduction-to-angular';
-}
+export class AppComponent {}
---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,10 @@
+// hello-world.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  imports: [],
+  templateUrl: './hello-world.component.html',
+  styleUrl: './hello-world.component.css',
+})
+export class HelloWorldComponent {}
