19f882266aadf7c572041b48809310a6952685eb
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2024-12-03 18:40:48 +0100

    perf: use the `OnPush` change detection strategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 // app.component.ts
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @Component({
@@ -7,6 +7,7 @@ import { HelloWorldComponent } from './hello-world/hello-world.component';
   imports: [HelloWorldComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   messages: readonly string[] = [];
--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,11 +1,18 @@
 // hello-world.component.ts
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-hello-world',
   imports: [],
   templateUrl: './hello-world.component.html',
   styleUrl: './hello-world.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HelloWorldComponent {
   @Input()
