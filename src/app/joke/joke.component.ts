fed2e168130d007d2d0f48905f910ce7ca216d91
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2024-12-10 19:57:16 +0100

    refactor: manage subscription using AsyncPipe

--- src/app/joke/joke.component.ts
+++ src/app/joke/joke.component.ts
@@ -1,32 +1,22 @@
 // joke.component.ts
+import { AsyncPipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { tap } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { JokeResponse } from '../joke-response';
 
 @Component({
   selector: 'app-joke',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './joke.component.html',
   styleUrl: './joke.component.css',
 })
 export class JokeComponent {
   http = inject(HttpClient);
 
-  jokeRequest$ = this.http.get<JokeResponse>(
-    'https://v2.jokeapi.dev/joke/Programming?safe-mode&type=single',
-  );
-  joke?: string;
-
-  constructor() {
-    this.jokeRequest$
-      .pipe(
-        tap((response) => {
-          this.joke = response.joke;
-        }),
-        takeUntilDestroyed(),
-      )
-      .subscribe();
-  }
+  joke$: Observable<string> = this.http
+    .get<JokeResponse>(
+      'https://v2.jokeapi.dev/joke/Programming?safe-mode&type=single',
+    )
+    .pipe(map((response) => response.joke));
 }
